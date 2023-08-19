29b2b9471688f09c65c3d2c401c154e5e52ade24
João Vítor Drumond <77032902+jao-124@users.noreply.github.com>
2023-08-18 22:44:50 -0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -11,15 +11,15 @@
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 
-/*----------------------->definição das portas de saída<------------------------------*/
+/*------------------------->DEFINIÇÃO DAS PORTAS DE SAÍDA<------------------------------*/
 /*#define GPIO_OUTPUT_IO_0    CONFIG_GPIO_OUTPUT_0    GPIO_OUTPUT_IO_0 = 0000000000000000000001000000000000000000
 #define GPIO_OUTPUT_IO_1    CONFIG_GPIO_OUTPUT_1    GPIO_OUTPUT_IO_1 = 0000000000000000000010000000000000000000    
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
 */
 #define GPIO_OUTPUT_IO_0 2
-#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0))
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0)) //Vetor dos pinos de saída
 
-/*----------------------->definição das portas de entrada<------------------------------*/
+/*----------------------->DEFINIÇÃO DAS PORTAS DE ENTRADA<------------------------------*/
 /*#define GPIO_INPUT_IO_0     CONFIG_GPIO_INPUT_0     GPIO_INPUT_IO_0 = 0000000000000000000000000000000000010000
 #define GPIO_INPUT_IO_1     CONFIG_GPIO_INPUT_1     GPIO_INPUT_IO_1 = 0000000000000000000000000000000000100000
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
@@ -27,29 +27,29 @@
 #define GPIO_INPUT_IO_0 21
 #define GPIO_INPUT_IO_1 22
 #define GPIO_INPUT_IO_2 23
-#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1) | (1ULL<<GPIO_INPUT_IO_2))
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1) | (1ULL<<GPIO_INPUT_IO_2))  //Vetor dos pinos de entrada
 
 
-/*----------------------->CRIAÇÃO DA FLAG DE INTERRUPÇÃO<------------------------------*/
+/*-------------------------->CRIAÇÃO DA FLAG DE INTERRUPÇÃO<---------------------------*/
 #define ESP_INTR_FLAG_DEFAULT 0
 
 /*DECLARANDO AS TAGS*/
 static const char* TAG_INFO_SYS_01 = "DADOS_SISTEMA";
 static const char* TAG_BOT_LED_02 = "BOT_LED";
 
-/*------------------>DECLARANDO A FILA<-------------------------*/
+/*---------------------------------->DECLARANDO A FILA<-------------------------------*/
 static QueueHandle_t gpio_evt_queue = NULL;
 
-/*------------>INTERRUPÇÃO<----------------------*/
+/*--------------------------------->INTERRUPÇÃO<-------------------------------------*/
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL); //Enviando endereço do pino que gerou a interrupção para a fila
 }
 
-/*--------------->STRUCT DE UMA TASK A SER CRIADA<--------------------*/
+/*-------------------------->STRUCT DE UMA TASK A SER CRIADA<-----------------------*/
 bool estado = false;
-static void gpio_task_button(void* arg)
+static void gpio_task_button(void* arg) //Tarefa associada aos botões
 {
     uint32_t io_num;
     for(;;) {
@@ -67,7 +67,7 @@ static void gpio_task_button(void* arg)
                 gpio_set_level(GPIO_OUTPUT_IO_0,!estado);
             }
             //printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
-            ESP_LOGI(TAG_BOT_LED_02,"GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+            ESP_LOGI(TAG_BOT_LED_02,"GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num)); //Printando os dados do pino e o nível lógico associado
             vTaskDelay(10);
         }
     }
@@ -75,7 +75,7 @@ static void gpio_task_button(void* arg)
 
 void app_main(void)
 {
-    /*PRINT DOS DADOS DO CHIP - PT1*/
+    /*----------------------->*PRINT DOS DADOS DO CHIP - PT1<----------------------*/
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -97,9 +97,10 @@ void app_main(void)
         return;
     }
 
-    /*CONFIGURAÇÃO DO GPIO - PT2*/
+    /*----------------------->*CONFIGURAÇÃO DO GPIO - PT2<-------------------------*/
 
-     //zero-initialize the config structure.
+    /*CONFIGURANDO AS SAÍDAS*/ 
+    //zero-initialize the config structure.
     gpio_config_t io_conf = {};
     //disable interrupt
     io_conf.intr_type = GPIO_INTR_DISABLE;
@@ -115,9 +116,10 @@ void app_main(void)
     //configure GPIO with the given settings
     gpio_config(&io_conf);
 
-    /*----------->INTERRUPÇÃO NA BORDA DE DESCIDA PARA TODOS OS PINOS DE SAÍDA<-------------*/
+    /*------>INTERRUPÇÃO NA BORDA DE DESCIDA PARA TODOS OS PINOS DE SAÍDA<--------*/
+    /*CONFIGURANDO AS ENTRADAS*/ 
     //interrupt of fallen edge
-    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    io_conf.intr_type = GPIO_INTR_NEGEDGE; //Interrupção na boda de descida
     //bit mask of the pins, use GPIO 0 here
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     //set as input mode
@@ -127,19 +129,19 @@ void app_main(void)
     /*CARREGAR CONFIGURAÇÕES NOS PINOS DE ENTRADA*/
     gpio_config(&io_conf);
 
-    /*---------->CRIAÇÃO DA FILA<----------------*/
+    /*------------------------------->CRIAÇÃO DA FILA<----------------------------*/
     //create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
 
-    /*------------>CRIAÇÃO DE UMA TASK ATRIBUÍDA À STRUCT CRIADA<-------------------*/
+    /*----------->CRIAÇÃO DE UMA TASK ATRIBUÍDA À STRUCT CRIADA<-----------------*/
     //start gpio task
     xTaskCreate(gpio_task_button, "gpio_task_button", 2048, NULL, 10, NULL);
 
-    /*---------------------->INSTALANDO A INTERRUPÇÃO<------------------------------*/
+    /*---------------------->INSTALANDO A INTERRUPÇÃO<--------------------------*/
     //install gpio isr service
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
 
-    /*---------------->ATRIBUIÇÃO DA INTERRUPÇÃO AOS PINOS DE ENTRADA<----------------------*/
+    /*---------->ATRIBUIÇÃO DA INTERRUPÇÃO AOS PINOS DE ENTRADA<----------------*/
     //hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
     gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
